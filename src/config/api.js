34ed04ce3f74a53d956cf24a1264137154f6c622
Chairul Iqbal Maulana <chairuliqbalmaulana@gmail.com>
2022-11-14 00:51:41 +0700

    update deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://server-waysbooks.herokuapp.com/api/v1",
 });
 
 export const setAuthToken = (token) => {
